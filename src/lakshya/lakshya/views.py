133ad7fcb0b0806c31e31e3304b7a2de45586105
srihari <srihari@boltell.com>
2012-11-13 15:06:02 +0530

    1. corrected the ordering bug in updates 2. cosmetic changes in navigation menu

--- src/lakshya/lakshya/views.py
+++ src/lakshya/lakshya/views.py
@@ -5,7 +5,7 @@ from accounts.models import Donation
 from django.db.models import Sum
 
 def get_home_page(request):
-    update_list = LakshyaUpdate.objects.filter(sorting__in = [1,2,3])  
+    update_list = LakshyaUpdate.objects.filter(sorting__in = [1,2,3]).order_by('sorting')  
     total_donation_amount = Donation.objects.all().aggregate(Sum("amount"))["amount__sum"]
     return render_to_response("index.html", 
                               RequestContext(request, {'update_list':update_list, 'total_donation_amount':total_donation_amount}))# Create your views here.
