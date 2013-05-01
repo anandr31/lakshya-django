f51d6c050c7355611606b87658954a7568368a46
Rashid Khan <rashood.khan@gmail.com>
2013-05-01 10:14:13 +0400

    Updated the Readme

--- src/lakshya/lakshya/settings_local.py
+++ src/lakshya/lakshya/settings_local.py
@@ -3,11 +3,13 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'lakshya',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'binfikrbol',                  # Not used with sqlite3.
+        'PASSWORD': 'Orange2020',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
-}
+}   
+
+
 
 merchant_id="M_thelaksh_10884"
 working_key="95o6bpj72771v3yo7s"
