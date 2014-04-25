907c121ae9370c002604d34c88fac592555ad8a3
Anand <anand@anand.(none)>
2014-04-26 01:29:10 +0530

    testing FB login button

--- src/lakshya/lakshya/urls.py
+++ src/lakshya/lakshya/urls.py
@@ -40,6 +40,7 @@ urlpatterns = patterns('',
     url(r'^corpus$', TemplateView.as_view(template_name="corpus.html")),
     url(r'^newsletter$', TemplateView.as_view(template_name="newsletter.html")),
     url(r'^applicants/?$', "accounts.views.donate_home", name='research_facilitator_applicants'),
+    url(r'^test_fb$', TemplateView.as_view(template_name="test_fb.html")),
    
     # url(r'^$', 'lakshya.views.home', name='home'),
     # url(r'^lakshya/', include('lakshya.foo.urls')),
