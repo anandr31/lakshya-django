c9d084c468974b3384f90b69c6ebc993f9c37416
Rashid Khan <rashood.khan@gmail.com>
2014-07-30 13:00:09 +0530

    Added views and URL for Hackathon

--- src/lakshya/hackathon/models.py
+++ src/lakshya/hackathon/models.py
@@ -5,8 +5,8 @@ class ProblemStatement(models.Model):
     name = models.CharField("Problem",max_length=100,blank=False)
 
     def __unicode__(self):
-        return  self.name;
-    
+        return  self.name
+
 class Participant(models.Model):
     name = models.CharField("Name",max_length=50,blank=False)
     email = models.EmailField("Email",blank=False)
@@ -15,5 +15,5 @@ class Participant(models.Model):
     team = models.CharField("Team",max_length=100,blank=False)
 
     def __unicode__(self):
-        return  self.name;
+        return  self.name
 
--- src/lakshya/hackathon/views.py
+++ src/lakshya/hackathon/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from forms import RegistrationForm
+from models import *
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'hackathon/index.html', {})
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        form.save()
+
+        return render(request,"hackathon/success.html",{})
+    else:
+        form = RegistrationForm()
+        return render(request,'project/register.html',{'form':form})
