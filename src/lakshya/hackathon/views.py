1737b4d364e7f8da66a026b9ff8eea1c2b19fd0f
kalyanvarma <kalyan123.india@gmail.com>
2015-04-26 15:07:25 +0530

    Hackathon page backend partial

--- src/lakshya/hackathon/views.py
+++ src/lakshya/hackathon/views.py
@@ -3,34 +3,35 @@ from django.http import HttpResponseRedirect
 from forms import RegistrationForm, RegForm
 from django.shortcuts import render_to_response, render, get_object_or_404
 from django.template.context import RequestContext
-from models import *
+from hackathon.models import *
 from django.views.generic.base import TemplateView
+from django.http.response import Http404
 
-# Create your views here.
+PREV_HACKATHON_PARTICIPANT_COUNTS = {1: 34, 2: 80, 3: 130}  # Hard coded for now since we dont have all the data
 
 
 def index(request):
     try:
         hackathon = Hackathon.objects.all().filter(is_active=True)[0]
     except Exception, e:
-        hackathon=None
+        hackathon = None
         #return render(request, 'hackathon/index.html');
     context = {}
     sponsors = Sponsors.objects.all()
     if hackathon is None:
-        context = {'sponsors':sponsors,'finished':'True','Message':'Stay tuned for updates!'}
-        return render(request, 'hackathon/index.html',context)
+        context = {'sponsors':sponsors, 'finished':'True', 'Message':'Stay tuned for updates!'}
+        return render(request, 'hackathon/index.html', context)
     sponsors = sponsors.filter(hackathon=hackathon)
-    context = {'hackathon':hackathon,'sponsors':sponsors}
+    context = {'hackathon':hackathon, 'sponsors':sponsors}
     return render(request, 'hackathon/index.html', context)
 
 def register(request):
     try:
         hackathon = Hackathon.objects.all().filter(is_active=True)[0]
     except Exception, e:
-        hackathon=None;
+        hackathon = None;
     if hackathon is None:
-        return render_to_response('hackathon/register.html', RequestContext(request,{'finished':'True'}))
+        return render_to_response('hackathon/register.html', RequestContext(request, {'finished':'True'}))
 
     if request.method == 'POST' and request.user.is_authenticated():
         form = RegForm(request.POST)
@@ -47,23 +48,23 @@ def register(request):
             tee = form.cleaned_data['tee']
             gender = form.cleaned_data['gender']
 
-            participant = Participant(name=name,mobile=mobile,team=team,email=email,problem=problem,year=year,course=course,
-                                      branch=branch,mess=mess,roll_no=roll_no,tee_shirt_size=tee,gender=gender,hackathon=hackathon)
+            participant = Participant(name=name, mobile=mobile, team=team, email=email, problem=problem, year=year, course=course,
+                                      branch=branch, mess=mess, roll_no=roll_no, tee_shirt_size=tee, gender=gender, hackathon=hackathon)
 
             participant.save()
 
-            return render_to_response('hackathon/success.html', RequestContext(request,{'name':name}))
+            return render_to_response('hackathon/success.html', RequestContext(request, {'name':name}))
         else:
-            render_to_response('hackathon/register.html',RequestContext(request,{'form':form}))
+            render_to_response('hackathon/register.html', RequestContext(request, {'form':form}))
     try:
         problem = ProblemStatement.objects.all().filter(is_active=True)[0]
-    except Exception,e:
+    except Exception, e:
         problem = None
     if problem is None:
         form = RegForm()
     else:
-        form = RegForm(initial = {'problem':problem})
-    return render(request,'hackathon/register.html',{'form':form})
+        form = RegForm(initial={'problem':problem})
+    return render(request, 'hackathon/register.html', {'form':form})
 
 def get_email(request):
     if request.user.is_superuser:
@@ -72,18 +73,18 @@ def get_email(request):
         for participant in participants:
             emails.append(participant.user.email)
 
-        return render(request,"hackathon/emails.html",{'emails':emails})
+        return render(request, "hackathon/emails.html", {'emails':emails})
     else:
         return HttpResponseRedirect('/admin/')
 
-def get_email_by_prob_statement(request,problem_id):
+def get_email_by_prob_statement(request, problem_id):
     if request.user.is_superuser:
         participants = Participant.objects.all().filter(problem_id=problem_id)
         emails = []
         for participant in participants:
             emails.append(participant.user.email)
 
-        return render(request,"hackathon/emails.html",{'emails':emails})
+        return render(request, "hackathon/emails.html", {'emails':emails})
     else:
         return HttpResponseRedirect('/admin/')
 
@@ -92,30 +93,30 @@ def problems(request):
         hackathon = Hackathon.objects.all().filter(is_active=True)[0]
     except Exception, e:
         hackathon = None
-    
+
     if hackathon is None:
         problem_sts = ProblemStatement.objects.all()
     else:
         problem_sts = ProblemStatement.objects.all().filter(hackathon=hackathon)
     context = {'problems':problem_sts}
-    return render(request, 'hackathon/problems.html',context)
+    return render(request, 'hackathon/problems.html', context)
 
-def get_problem_statement(request,problem_id):
-    problem = get_object_or_404(ProblemStatement,pk=problem_id)
+def get_problem_statement(request, problem_id):
+    problem = get_object_or_404(ProblemStatement, pk=problem_id)
     context = {'problem':problem}
-    return render(request,'hackathon/problem_statement.html',context)
+    return render(request, 'hackathon/problem_statement.html', context)
 
 def faq(request):
     hackathon = Hackathon.objects.all().filter(is_active=True)[0]
     if hackathon is None:
-        return render(request,'hackathon/faq.html')
+        return render(request, 'hackathon/faq.html')
     else:
-        return render(request,'hackathon/faq.html',{'hackathon':hackathon})
+        return render(request, 'hackathon/faq.html', {'hackathon':hackathon})
 
 def student_details(request):
     if request.user.is_superuser:
         participants = Participant.objects.all()
-        return render(request,'hackathon/students.html',{'students':participants})
+        return render(request, 'hackathon/students.html', {'students':participants})
     else:
         return HttpResponseRedirect('/admin')
 
@@ -123,6 +124,32 @@ def student_details(request):
 class HackathonHomeView(TemplateView):
     template_name = 'hackathon/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(HackathonHomeView, self).get_context_data(**kwargs)
+        context['hackathons'] = Hackathon.objects.all().order_by('start_time')
+        context['mentors'] = Mentor.objects.all().order_by('-id')[:6]
+        context['sponsors'] = Sponsor.objects.all().order_by('-id')[:6]
+        return context
+
 
 class HackathonDetailView(TemplateView):
     template_name = 'hackathon/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HackathonDetailView, self).get_context_data(**kwargs)
+        try:
+            hackathon_id = kwargs.get('id', None)
+            context['hackathon'] = hackathon = Hackathon.objects.get(id=hackathon_id)
+        except (ValueError, Hackathon.DoesNotExist):
+            raise Http404
+
+        if hackathon.id in PREV_HACKATHON_PARTICIPANT_COUNTS:
+            context['participant_count'] = PREV_HACKATHON_PARTICIPANT_COUNTS[hackathon.id]
+        else:
+            context['participant_count'] = hackathon.participants.count()
+
+        context['problem_statements'] = hackathon.problem_statements.all()
+        context['mentors'] = hackathon.mentors.all()
+        context['sponsors'] = hackathon.sponsors.all()
+
+        return context
