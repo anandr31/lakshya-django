7007e5db05e3cd6857ca3d3a7e9b40f4e9891a9b
Sarath Kumar Somana <sarath@almabase.com>
2015-04-26 17:19:24 +0530

    Partners page - database driven completed

--- src/lakshya/utils/views.py
+++ src/lakshya/utils/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render_to_response
 from django.contrib.admin.views.decorators import staff_member_required
 from django.template import RequestContext
+from django.views.generic import TemplateView
 
 from utils.models import LakshyaUpdate
-from utils.models import LakshyaTestimonial
+from utils.models import LakshyaTestimonial, Partner
 from accounts.models import Donation
 from people.models import Person
 from django.db.models.aggregates import Sum
 from django.contrib.auth.models import User
 
+
 def get_updates(request):
-    update_list = LakshyaUpdate.objects.order_by('-date_of_entry')  
+    update_list = LakshyaUpdate.objects.order_by('-date_of_entry')
     print update_list
-    return render_to_response("all_updates.html", 
-                              RequestContext(request, {'update_list':update_list}))# Create your views here.
+    return render_to_response("all_updates.html",
+                              RequestContext(request, {'update_list': update_list}))  # Create your views here.
+
 
 def get_testimonials(request):
-    testimonial_list = LakshyaTestimonial.objects.order_by('-sorting')  
+    testimonial_list = LakshyaTestimonial.objects.order_by('-sorting')
     print testimonial_list
-    return render_to_response("all_testimonials.html", 
-                              RequestContext(request, {'testimonial_list':testimonial_list}))# Create your views here.
-    
+    return render_to_response("all_testimonials.html",
+                              RequestContext(request, {'testimonial_list': testimonial_list}))  # Create your views here.
+
+
 @staff_member_required
 def get_donation_details_for_analytics(request):
     context = {}
@@ -28,24 +32,37 @@ def get_donation_details_for_analytics(request):
     if request.GET and request.GET["separator"]:
         separator = request.GET["separator"]
     context["separator"] = separator
-    
-    donor_details_list = [] #list of tuples - name, batch, branch, amount, last Donated on
+
+    # list of tuples - name, batch, branch, amount, last Donated on
+    donor_details_list = []
     for temp_dict in Donation.objects.values('donor').annotate(total=Sum('amount')):
         donor_id = temp_dict["donor"]
         total = temp_dict["total"]
         donor = Person.objects.get(id=donor_id)
-        last_donated_on = Donation.objects.filter(donor = donor).order_by("-date_of_donation")[0].date_of_donation
-        donor_details = (donor.name, donor.year_of_passing, donor.get_department_display, total, last_donated_on.strftime("%d-%m-%Y"))
+        last_donated_on = Donation.objects.filter(donor=donor).order_by(
+            "-date_of_donation")[0].date_of_donation
+        donor_details = (donor.name, donor.year_of_passing,
+                         donor.get_department_display, total, last_donated_on.strftime("%d-%m-%Y"))
         donor_details_list.append(donor_details)
-    
-    context['donor_details_list'] = donor_details_list
-    return render_to_response("get-donation-details-for-analytics.html", 
-                              RequestContext(request, context))   
-    
-@staff_member_required
-def get_donor_emails_for_campaigns(request):    
-    emails = [ user.email for user in User.objects.filter(person__donation__isnull=False).distinct() if user.email]
-    return render_to_response("get-donor-emails-for-campaigns.html", 
-                              RequestContext(request, {"email_addresses" : ", ".join(emails)}))
-             
 
+    context['donor_details_list'] = donor_details_list
+    return render_to_response("get-donation-details-for-analytics.html",
+                              RequestContext(request, context))
+
+
+@staff_member_required
+def get_donor_emails_for_campaigns(request):
+    emails = [user.email for user in User.objects.filter(
+        person__donation__isnull=False).distinct() if user.email]
+    return render_to_response("get-donor-emails-for-campaigns.html",
+                              RequestContext(request, {"email_addresses": ", ".join(emails)}))
+
+
+class PartnersView(TemplateView):
+    template_name = "lakshya/partners.html"
+
+    def get_context_data(self, **kwargs):
+        context = TemplateView.get_context_data(self, **kwargs)
+        partners = Partner.objects.all().order_by('priority')
+        context['partners'] = partners
+        return context
