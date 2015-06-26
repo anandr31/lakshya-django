3a0e3b6d034e4368965ff0c7db7b7e5eb41b4daf
anandr31 <anandr31@gmail.com>
2015-06-26 17:57:03 +0530

    Fix PAN card not stored issue

--- src/lakshya/accounts/views.py
+++ src/lakshya/accounts/views.py
@@ -96,7 +96,7 @@ def payment_redirect(request):
             amount = form.cleaned_data['amount']
             email_address = form.cleaned_data['email_address']
             email_receipt = form.cleaned_data['email_receipt']
-            pan_card = form.cleaned_data['pan_number']
+            pan_card = form.cleaned_data['pan_card']
 	    if referrer_url == '/applicants' and notes <> "":
 	    	notes = "Internship Sponroship for "+notes
             pt = PaymentTemp.objects.create(amount=amount, email_address=email_address, email_receipt=email_receipt, pan_card=pan_card)
