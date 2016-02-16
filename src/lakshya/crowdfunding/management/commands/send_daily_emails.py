f0016b081d60a960f388d31803fe76c3f8da3acb
anandr31 <anandr31@gmail.com>
2016-02-16 17:31:32 +0530

    send daily mails check

--- src/lakshya/crowdfunding/management/commands/send_daily_emails.py
+++ src/lakshya/crowdfunding/management/commands/send_daily_emails.py
@@ -1,6 +1,6 @@
 # A management command to send emails to authors and pledgers based on the status of the projects.
 # This will be run using a cron job once every day.
-
+from datetime import datetime
 from lakshya.util import send_cron_job_emails
 from django.core.management.base import BaseCommand
 
@@ -8,4 +8,6 @@ class Command(BaseCommand):
 	args = ''
 	help = 'Sends emails to pledgers and authors based on the status of the project'
 	def handle(self, *args, **options):
+		self.stdout.write("Job starts at [" + datetime.now().strftime('%d/%m/%Y %H:%M:%S') + "]")
 		send_cron_job_emails()
+		self.stdout.write("Job ends at [" + datetime.now().strftime('%d/%m/%Y %H:%M:%S') + "]")
