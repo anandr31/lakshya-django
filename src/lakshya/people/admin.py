bd9ca249ab690aef3edc9f9d0878c33fd0d7b79a
anandr31 <anandr31@gmail.com>
2016-02-24 16:12:44 +0530

    adding country to admin view in persons page

--- src/lakshya/people/admin.py
+++ src/lakshya/people/admin.py
@@ -10,7 +10,7 @@ class PersonOptions(admin.ModelAdmin):
                                                   ('billing_state', 'billing_postal_code'), ('billing_country', 'contact_number', 'pan_number',), ]}), 
                  ("Alumni Details", {"fields" : [('is_nitw_alumni',), ('course', 'department', 'year_of_passing',)]}), ]
     
-    list_display = ('user', 'is_nitw_alumni', 'course', "department", "year_of_passing",)
+    list_display = ('user', 'is_nitw_alumni', 'course', "department", "year_of_passing","billing_country")
     list_filter = ('is_nitw_alumni', 'course', "department", "year_of_passing", )
     search_fields = ('user__first_name', )
     raw_id_fields = ('user', )
